75228fb8f9b04bc78e088da96aa09676d623e11c
Marco H <mahe0046@stud.hs-kl.de>
2020-05-02 22:11:29 +0200

    GoogleVR-Cardboard Version

---
+++ Assets/GvRChangeGameState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GvRChangeGameState : MonoBehaviour
+{
+    public int changeState;
+
+    private void Awake()
+    {
+        changeState = 0;
+    }
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void ChangeGameState()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+        if (changeState == 3) changeState = 0;
+        GameObject.Find("ToH").GetComponent<GameToH>().ChangingGameState(changeState);
+        changeState++;
+    }
+}
