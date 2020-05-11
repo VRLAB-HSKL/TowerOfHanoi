45dcac0933cf8750abf9525cf135fe7239e220e3
Marco H <mahe0046@stud.hs-kl.de>
2020-05-11 16:09:28 +0200

    add BuildScript

--- Assets/Scripts/Build/BuildScript.cs
+++ Assets/Scripts/Build/BuildScript.cs
@@ -1,11 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEditor;
 
+/// <summary>
+/// Crossplatform Buildscript
+/// </summary>
 public class BuildScript
 {
-    static void PerformBuild()
+    /// <summary>
+    /// Build KeyBoard / Mouse WindowsX64
+    /// </summary>
+    static void PerformBuildWindowsStandaloneX64()
     {
+        string[] defaultScene = { "Assets/Scenes/ToH-PCStandalone.unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/WinX64",
+            BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
 
+    /// <summary>
+    /// Build VivePro WindowsX64
+    /// </summary>
+    static void PerformBuildWindowsVivePro()
+    {
+        string[] defaultScene = { "Assets/Scenes/ToH-VivePro.unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/WinX64",
+            BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    /// <summary>
+    /// Build ViveFocus Android
+    /// </summary>
+    static void PerformBuildAndroidViveFocus()
+    {
+        string[] defaultScene = { "Assets/Scenes/ToH-ViveFocus.unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/Android",
+            BuildTarget.Android, BuildOptions.None);
+    }
+
+    /// <summary>
+    /// Build Cardboard Android
+    /// </summary>
+    static void PerformBuildAndroidCardboard()
+    {
+        string[] defaultScene = { "Assets/Scenes/ToH-Cardboard.unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/Android",
+            BuildTarget.Android, BuildOptions.None);
     }
 }
